463b8188e92c01f5d527d4608c60a3dd4f3096d8
olaitanade <olaitan.adetayo@gmail.com>
2021-06-11 03:53:30 +0100

    added deposit method

--- src/main/java/com/example/bankapplication/controller/BankApplicationRestController.java
+++ src/main/java/com/example/bankapplication/controller/BankApplicationRestController.java
@@ -1,6 +1,7 @@
 package com.example.bankapplication.controller;
 
 import com.example.bankapplication.model.request.Credential;
+import com.example.bankapplication.model.request.Deposit;
 import com.example.bankapplication.service.IBankApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +26,10 @@ public class BankApplicationRestController {
     @RequestMapping(value = "/account_statement", method = RequestMethod.POST)
     public ResponseEntity<?> accountStatement(@RequestBody Credential credential) {
         return bankApplicationService.getAccountStatement(credential);
+    }
+
+    @RequestMapping(value = "/deposit", method = RequestMethod.POST)
+    public ResponseEntity<?> deposit(@RequestBody Deposit deposit) {
+        return bankApplicationService.deposit(deposit);
     }
 }
