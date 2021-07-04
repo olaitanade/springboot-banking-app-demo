7673ad86fbb45c44af92c8a92c2f879a965beb78
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2021-07-04 21:23:08 +0100

    updated

--- src/main/java/com/example/bankapplication/BankApplication.java
+++ src/main/java/com/example/bankapplication/BankApplication.java
@@ -5,18 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class BankApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
@@ -27,5 +18,4 @@ public class BankApplication {
 			repository.initialize();
 		};
 	}
-
 }
